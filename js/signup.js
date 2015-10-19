95006e96fbe2d562fa1eef332ad1030b415d25d9
chrisrp <christianrp@users.noreply.github.com>
2015-10-18 18:08:17 -0700

    populated state list using javascript

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,18 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+
+document.addEventListener("DOMContentLoaded", populateStates);
+
+function populateStates () {
+    var signup = document.getElementById("signup");
+    var state = signup.elements.state;
+    
+    for (var i = 0; i < usStates.length; i++) {
+        var option = document.createElement("OPTION");
+        option.innerHTML = usStates[i].name;
+        option.value = usStates[i].value;
+        state.appendChild(option);
+    }
+}
+
